24574d50d8b73d57a2b9789f7ee2976a5b357121
huynq1992hpvn <huynq1992hpvn@gmail.com>
2023-05-15 17:32:54 +0700

    upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace doitien
+{
+    class Program 
+    {
+        static void Main(string[] agrs)
+        {
+            double rate = 23000;
+            double tienusd;
+            double tienVND;
+            Console.Write("nhap so USD ");
+            tienusd = double.Parse(Console.ReadLine());
+            tienVND = tienusd * rate;
+            Console.WriteLine("Doi ra tien Viet la " + tienVND);
+        }
+    }
+}
